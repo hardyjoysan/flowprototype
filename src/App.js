ce01f7e72c21efe173ea780ba8323c0fb0af2a68
Hardy Joysan <hardyjoysan@gmail.com>
2018-07-03 10:44:12 +0530

    svg design updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import React, { Component } from 'react';
 
-import './App.css';
-
 class App extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      isPasswordPage : false
+      isModelOneView : false
     };
-
-    this.enablePasswordPage  = this.enablePasswordPage.bind(this);
+    this.switchViewHandle  = this.switchViewHandle.bind(this);
   }
 
-  enablePasswordPage() {
+  switchViewHandle() {
     this.setState({
-      isPasswordPage : true
+      isModelOneView : true
     });
   }
 
-  showPasswordPage() {
-    this.setState({showPassword: true });
-  }
-
   render() {
-
-    const Circle1 = (
-      <svg height="300" width="300" onClick={this.enablePasswordPage}>
-        <circle cx="150" cy="150" r="145" stroke="black" strokeWidth="5" fill="#fff" />
+    const ModalOne = (
+      <svg xmlns="http://www.w3.org/2000/svg" width="800" height="800">
+        <g onClick={this.switchViewHandle}>
+          <ellipse ry="300" rx="300" cy="400" cx="400" stroke-width="5" stroke="#fff" fill="#555"></ellipse>
+          <ellipse ry="80" rx="80" cy="400" cx="200" stroke-width="3" stroke="#fff" fill="#555"></ellipse>
+          <ellipse ry="150" rx="150" cy="400" cx="500" stroke-width="3" stroke="#fff" fill="#555"></ellipse>
+          <line id="svg_5" y2="400" x2="280" y1="400" x1="350" stroke-width="3" stroke="#fff"></line>
+        </g>
       </svg>
     );
 
-    const Circle2 = (
+    const ModalTwo = (
       <svg height="300" width="300">
         <circle cx="150" cy="150" r="145" stroke="black" strokeWidth="5" fill="red" />
       </svg>
@@ -40,10 +36,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        { this.state.isPasswordPage ? Circle2 : Circle1 }
+        { this.state.isModelOneView ? ModalTwo : ModalOne }
       </div>
     );
-
   }
 }
 
