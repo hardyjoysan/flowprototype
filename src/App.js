7689868f862744804ebd3f17a5574da0d64154fa
Hardy Joysan <hardyjoysan@gmail.com>
2018-07-13 11:30:00 +0530

    Removed CongressionDistrict component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './css/App.css';
 
-import CongressionalDistricts from './CongressionalDistricts';
-
 class App extends Component {
   render() {
     return (
         <div className="App">
             <svg width="960" height="600">
-                <CongressionalDistricts width={960} height={600} />
+                
             </svg>
       </div>
     );
