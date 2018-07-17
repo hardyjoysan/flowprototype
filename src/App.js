e4ed893a9f9c702ac20e0c5aea6176fc2f4dd264
Hardy Joysan <hardyjoysan@gmail.com>
2018-07-17 16:43:43 +0530

    d3.forceSimulation added to layout

--- src/App.js
+++ src/App.js
@@ -8,42 +8,52 @@ class App extends Component {
   componentDidMount(){
 
     const width = window.frameElement ? 960 : window.innerWidth,
-      height = window.frameElement ? 600 : window.innerHeight;
+      height = window.frameElement ? 600 : window.innerHeight,
+      margin = {top: -5, right: -5, bottom: -5, left: -5};
 
-    const data = d3.range(20).map(function() { return [Math.random() * width, Math.random() * height]; });
-
-    const svg = d3.select(".App")
-              .append("svg")
-              .attr("width", width)
-              .attr("height", height);
-
-    const g = svg.append("g");
-
-    g.append("rect")
+      const svg = d3.select(".App")
+      .append("svg")
       .attr("width", width)
-      .attr("height", height)
-      .call(d3.zoom().scaleExtent([0.3, 10]).on("zoom", zoomed));
+      .attr("height", height);
 
-    const view = g.append("g")
+    const view = svg.append("g")
                 .attr("class", "view");
 
-    view.selectAll("circle")
-        .data(data)
-        .enter().append("circle")
-        .attr("transform", function(d) { return "translate(" + d + ")"; })
-        .attr("r", 50);
+    //const data = [ { "source" : "A", "target" : "B" }, { "source" : "B", "target" : "C" }, { "source" : "C", "target" : "D" }, { "source" : "D", "target" : "E" }, { "source" : "E", "target" : "F" }, { "source" : "F", "target" : "G" }, { "source" : "G", "target" : "H" }, { "source" : "H", "target" : "I" }, { "source" : "I", "target" : "J" }, { "source" : "J", "target" : "A" } ] ; 
 
-    function zoomed() {
-      view.attr('transform', 'translate(' + d3.event.transform.x + ',' + d3.event.transform.y + ') scale(' + d3.event.transform.k + ')');
-    }
+    var nodes = [{name: 'Alice'}, {name: 'Bob'}, {name: 'Eve'}],
+        links = [{source: 0, target: 1}, {source: 2, target: 0}];
+
+    var force = d3.forceSimulation(nodes)
+    .force('link', d3.forceLink())
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(width / 2, height / 2));
+    
+
+    force.nodes(nodes)
+        .force("charge", d3.forceManyBody())
+        .force("link", d3.forceLink(links));
+
+    var node = view.selectAll('circle')
+                .data(nodes).enter().append('circle')
+                .attr('r', 5);
+
+    var link = view.selectAll('line')
+                  .data(links).enter().append('line');
+
+    force.on('tick', function() {
+      node.attr('cx', function(d) {return d.x;})
+          .attr('cy', function(d) {return d.y;});
+      link.attr('x1', function(d) {return d.source.x;})
+          .attr('y1', function(d) {return d.source.y;})
+          .attr('x2', function(d) {return d.target.x;})
+          .attr('y2', function(d) {return d.target.y;});
+    });
   }
-
-  
-  
 
   render() {
     return (
-        <div className="App">
+      <div className="App">
 
       </div>
     );
