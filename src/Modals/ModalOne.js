424defd6511c01484071ae6ab007e5207860448c
hardyjoysan <hardyjoysan@gmail.com>
2018-07-27 18:04:17 +0530

    Circle created with JSON data nodes

--- src/Modals/ModalOne.js
+++ src/Modals/ModalOne.js
@@ -8,16 +8,30 @@ class ModalOne extends Component {
         const width = window.frameElement ? 960 : window.innerWidth - 100,
             height = window.frameElement ? 600 : window.innerHeight - 100;
 
+        const data = [{
+            "name":"Acme Inc"
+        }];
+
+        data.forEach(function(node){
+            node.cx = width/2;
+            node.cy = height/2;
+            node.r = Math.min(width, height)/2.5;
+        })
+        
         const svg = d3.select(".modalOne")
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height);
 
-        svg.append("g")
-        .append("circle")
-        .attr("r", Math.min(width, height)/2.5)
-        .attr("cx", width/2)
-        .attr("cy", height/2)
+        const node = svg.selectAll('.node')
+                    .data(data)
+                    .enter().append("g")
+                    .attr("class", "node");
+
+        node.append("circle")
+            .attr('r', function(d) { return d.r; })
+            .attr('cx', function(d) { return d.cx; })
+            .attr('cy', function(d) { return d.cy; });
 
 
     }
