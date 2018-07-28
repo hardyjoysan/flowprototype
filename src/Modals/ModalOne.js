83245f0fb7a55e940c9dbbeb530c4562561e5989
hardyjoysan <hardyjoysan@gmail.com>
2018-07-28 19:49:39 +0530

    ModalOne created circles inside circle with angle calculation

--- src/Modals/ModalOne.js
+++ src/Modals/ModalOne.js
@@ -9,31 +9,66 @@ class ModalOne extends Component {
             height = window.frameElement ? 600 : window.innerHeight - 100;
 
         const data = [{
-            "name":"Acme Inc"
+            "company":"Acme Inc",
+            "children":[
+                {
+                    "division":"Stockholm",
+                    "children":[{"team": "Optimus Prime"},{"team": "Bumblebee"},{"team": "Starscream"}]
+                },
+                {
+                    "division": "Gothenburg",
+                    "children":[{"team": "Megatron"}]
+                }
+            ]
         }];
 
-        data.forEach(function(node){
-            node.cx = width/2;
-            node.cy = height/2;
-            node.r = Math.min(width, height)/2.5;
-        })
+        function nodeCalculation(width, height, data) {
+            data.forEach(function(node){
+                node.cx = width/2;
+                node.cy = height/2;
+                node.r = Math.min(width, height)/2.5;
+
+                if (node.children) {
+                    for (var i = 0; i < node.children.length; i++) {
+                        var angle = (i / (node.children.length/2)) * Math.PI;
+
+                        var child_r = node.r / (node.children.length * 2);
+                        if (node.children[i].children && node.children[i].children.length > 1) {
+                            child_r = child_r + 50;
+                        }
+
+                        var orbit_r = node.r - child_r - 30;
+                        node.children[i].cx = node.cx + orbit_r * Math.cos(angle);
+                        node.children[i].cy = node.cy + orbit_r * Math.sin(angle);
+                        node.children[i].r = child_r;
+                    }
+                }
+            });
+        }
+
+        nodeCalculation(width, height, data);
         
         const svg = d3.select(".modalOne")
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height);
+        
+        function nodeCreation(data) {
+            data.forEach(function(node){
+                svg.append("g")
+                    .attr("class", "node")
+                    .append("circle")
+                    .attr('r', node.r)
+                    .attr('cx', node.cx)
+                    .attr('cy', node.cy);
 
-        const node = svg.selectAll('.node')
-                    .data(data)
-                    .enter().append("g")
-                    .attr("class", "node");
+                    if (node.children) {
+                        nodeCreation(node.children);
+                    }
+            });
+        }
 
-        node.append("circle")
-            .attr('r', function(d) { return d.r; })
-            .attr('cx', function(d) { return d.cx; })
-            .attr('cy', function(d) { return d.cy; });
-
-
+        nodeCreation(data);
     }
 
     render(){
