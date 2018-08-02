da155817f6521f0e6967efd02e88fb975be31b76
Hardy Joysan <hardyjoysan@gmail.com>
2018-08-02 17:26:55 +0530

    ModalThree apis circles and lines append to teams

--- src/Modals/ModalThree.js
+++ src/Modals/ModalThree.js
@@ -9,34 +9,74 @@ class ModalThree extends Component {
 
         var data =
             [
-                { "team": "Optimus Prime" },
-                { "team": "Bumblebee" },
-                { "team": "Starscream" }
+                { "team": "Optimus Prime", "children": [{"api":"API1"}, {"api":"API2"}, {"api":"API3"}] },
+                { "team": "Bumblebee", "children": [{"api":"API1"}, {"api":"API2"}, {"api":"API3"}] },
+                { "team": "Starscream", "children": [{"api":"API1"}, {"api":"API2"}] },
             ];
 
-            var dataLinks = [
-                { source: 0, target: 1 },
-                { source: 1, target: 2 },
-                { source: 2, target: 0 },
-            ];
+            d3.forceSimulation().on('tick', tick);
 
-            const force = d3.forceSimulation()
-                            .nodes(data)
-                            .force("link", d3.forceLink(dataLinks).distance(Math.min(width, height)/1.5))
-                            .force('charge', d3.forceManyBody(-20))
-                            .force('center', d3.forceCenter(width / 2, height / 2))
-                            .force('collide', d3.forceCollide(100))
-                            .force('x', d3.forceX(width / 2).strength(0.5))
-                            .force('y',  d3.forceY(height / 2).strength(0.5))
-                            .on('tick', tick);
+            var i = 0;
+            data.links = [];
+            data.forEach(function(node) {
+                var angle = (i / (data.length / 2)) * Math.PI;
+                node.x = (width / 2) + 200 * Math.cos(angle);
+                node.y = (height / 2) + 200 * Math.sin(angle);
+                if (i === data.length - 1) {
+                    data.links[i] = { source: data[i], target: data[0] };
+                } else {
+                    data.links[i] = { source: data[i], target: data[i + 1] };
+                }
+                
+                if (node.children) {
+                    var nodechild = node.children;
+                    node.links = [];
+                    var j = 0;
+                    nodechild.forEach(function(child){
+                        var angle = (j / (nodechild.length / 2)) * Math.PI;
+                        child.x = node.x + 160 * Math.cos(angle);
+                        child.y = node.y + 160 * Math.sin(angle);
+                        child.r = 10;
+                        node.links[j] = { source: node, target: child };
+                        j++;
+                    });
+                }
+                i++;
+            });
 
             var svg = d3.select(".modalThree").append("svg")
                 .attr("width", width)
                 .attr("height", height);
 
+            var teams = data;
+
+            teams.forEach(function (apis) {
+                if (apis.children) {
+                    var x = 0;
+                    svg.selectAll(".apiline"+x)
+                    .data(apis.links)
+                    .enter()
+                    .append("line")
+                    .attr("class", ".apiline"+x)
+                    .attr("x1", function(d) { return d.source.x; })
+                    .attr("y1", function(d) { return d.source.y; })
+                    .attr("x2", function(d) { return d.target.x; })
+                    .attr("y2", function(d) { return d.target.y; });
+
+                    apis.children.forEach(function(api) {
+                        const g_t = svg.append('g').attr("class", "api");
+                        g_t.append('circle')
+                        .attr('r', api.r)
+                        .attr('cx', api.x)
+                        .attr('cy', api.y);
+                    })
+                    x++;
+                }
+                
+            });
 
             var links = svg.selectAll(".link")
-                    .data(dataLinks)
+                    .data(data.links)
                     .enter().append("line")
                     .attr("class", "link");
 
@@ -44,12 +84,7 @@ class ModalThree extends Component {
                     .data(data)
                     .enter().append("circle")
                     .attr("class", "node")
-                    .attr("r", 40)
-                    .call(d3.drag()
-                        .on("start", dragstarted)
-                        .on("drag", dragged)
-                        .on("end", dragended)
-                    );
+                    .attr("r", 40);
 
             var text = svg.selectAll(".text")
                             .data(data)
@@ -58,7 +93,9 @@ class ModalThree extends Component {
                             .attr("class", "text")
                             .attr('width', 150);
 
-            text.append('xhtml:h3').attr('class', 'header').style("font-size", "14px").text(function (d) { return d.team; })
+            text.append('xhtml:h3').attr('class', 'header')
+                .style("font-size", "14px")
+                .text(function (d) { return d.team; });
 
             function tick() {
                 links.attr("x1", function (d) { return d.source.x; })
@@ -72,30 +109,11 @@ class ModalThree extends Component {
                 text.attr("x", function (d) { return d.x - 75; })
                     .attr("y", function (d) { return d.y - 115; });
             }
-
-            function dragstarted(d) {
-                if (!d3.event.active) force.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-            } 
-           
-            function dragged(d) {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-            }
-          
-            function dragended(d) {
-                if (!d3.event.active) force.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-            }
     }
 
     render(){
         return(
-            <div className="modalThree">
-                  
-            </div>
+            <div className="modalThree"></div>
         );
     }
 }
